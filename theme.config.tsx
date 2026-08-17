cb24045177e592c141837eee89fcd85d398a3146
triath <archbirddev@gmail.com>
2026-08-17 12:09:59 -0700

    add docs to analytics

--- theme.config.tsx
+++ theme.config.tsx
@@ -22,6 +22,9 @@ const config: DocsThemeConfig = {
   head: (
     <>
       <link rel="icon" type="image/png" href="/images/logo_t.png" />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-1N6KVQV3HQ"></script>
+      <script dangerouslySetInnerHTML={{ __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-1N6KVQV3HQ');` }} />
+      <script defer src="https://app.rybbit.io/api/script.js" data-site-id="8404"></script>
     </>
   )
 
